18be045919db99662fdf09bb6cc9fff354a419d6
Farid Saemi <farid.saemi@tamu.edu>
2020-09-03 18:08:54 -0500

    ESC sizing, battery prediction
    added new functions to size ESC & battery, simulate battery discharge and accompanying documentation

--- evpy/evpy.py
+++ evpy/evpy.py
@@ -3,12 +3,13 @@
 ########################
 
 import numpy as np
+import scipy.integrate
 
 ##########################
 #### Library functions ###
 ##########################
 
-def motor_pred(V,d,Rm,kt,I0,w):
+def motor_pred(w,V,d,Rm,kt,I0):
     """
     predict motor performance for given specs
     
@@ -20,6 +21,8 @@ def motor_pred(V,d,Rm,kt,I0,w):
     
     INPUTS
     ------
+    w : ndarray (float), rads/sec
+        range of motor speed
     V : float, Volts
         voltage of the DC bus
     d : float, non-dim
@@ -30,8 +33,6 @@ def motor_pred(V,d,Rm,kt,I0,w):
         torque constant of motor
     I0 : float, Amps
         no-load current of motor 
-    w : ndarray (float), rads/sec
-        range of motor speed
     
     OUTPUTS
     -------
@@ -66,7 +67,7 @@ def motor_pred(V,d,Rm,kt,I0,w):
     
     return T,P_out,I,P_in,n
 
-def motor_size(T,x,shear=5500):
+def motor_size(T,x,shear=5500.0):
     """
     Size a motor for a given torque, aspect ratio
     
@@ -125,9 +126,35 @@ def motor_size(T,x,shear=5500):
     
     return m_tot,U_tot,Do,Lo,km
 
-def esc_pred(V,d,f_pwm,Ron,Ton,Im,Pm):
+def esc_pred(Im,Pm,V,d,f_pwm=8e3,Ron=10e-3,Ton=1e-6):
     """
     predict ESC losses given specs and motor performance
+    
+    INPUTS
+    ------
+    Im : ndarray (float), Amps
+        the current pulled by the motor
+    Pm : ndarray (float), Watts
+        the power pulled by the motor
+    V : float, Volts
+        the input (DC) voltage to the ESC
+    d : float, non-dim
+        the non-dimensional throttle setting (duty ratio)
+    f_pwm : float, Hertz
+        the switching frequency of the ESC, about 8-32 kHz
+    Ron : float, Ohms
+        the R_ds_ON measure of the MOSFETs in the ESC, about 5-20 mOhms
+    Ton : float, s (seconds)
+        the transition period of the MOSFETs, about 1 microsecond
+        
+    OUTPUTS
+    -------
+    I_dc : ndarray (float), Amps
+        the current draw of the ESC
+    P_dc : ndarray (float), Watts
+        the power draw of the ESC
+    n : ndarray (float), non-dim
+        the efficiency of the ESC
     """
     # compute copper losses
     P_L_co = 2*Ron*Im**2 #[W]
@@ -142,3 +169,161 @@ def esc_pred(V,d,f_pwm,Ron,Ton,Im,Pm):
     I_dc[I_dc<0.0] = np.nan
     
     return I_dc,P_dc,n
+
+def esc_size(P_req,sf=2.0):
+    """
+    predict esc size, mass given cont. power
+    
+    predict the esc volume and mass using a purely empirical fit
+    empirical data collected from 3 KDE, Castle, and HobbyWing data
+    nearly uniform trends among all three datasets    
+    
+    P = 36.203m where [P] = Watts, [m] = grams
+    P = 52.280U where [P] = Watts, [U] = cm^3
+    
+    INPUTS
+    ------
+    P_req : float, Watts
+        the required continuous power output of the ESC
+    sf : float, non-dim (optional)
+        a safety factor for the prediction
+        hobby rule of thumb is 2.0
+    
+    OUTPUTS
+    -------
+    m : float, kg
+        the mass of the ESC
+    U : float, m^3
+        the volume of the ESC
+    """
+    m = sf*(2.762e-5)*P_req #[kg]
+    U = sf*(1.913e-8)*P_req #[m^3]
+    
+    return m,U
+
+def batt_pred(I_load,t_hr,Q_Ah,R_int,n_ser=1,n_prll=1,pkrt=1.2):
+    """
+    predict voltage at a given time under a given load
+    
+    predict the entire pack's instantaneous terminal voltage under load
+    uses empirical state-of-charge curve fit obtained from Chen and Mora
+    https://doi.org/10.1109/TEC.2006.874229
+      
+    INPUTS
+    ------
+    I_load : float, Amps
+        the current draw at the battery terminals
+    t : float, hours
+        the instant in time
+    Q_Ah : float, Amp*hr
+        the rated capacity of the battery unit
+    R_int : float, Ohms
+        the internal resistance of the battery unit
+        in the tens of mili-Ohms range
+    n_ser : int, non-dim (optional)
+        the number of battery units in series
+    n_prll : int, non-dim (optional)
+        the number of battery units in parallel
+    pkrt : float, non-dim (optional)
+        the Peukert constant of the battery
+    
+    OUTPUTS
+    -------
+    V_term : float, Volts
+        the output voltage of the battery
+    dod : float, non-dim
+        the depth of discharge of the battery (percent depleted)
+    soc : float, non-dim
+        the state of charge of the battery (percent remaining)
+            
+    SPECIAL NOTES
+    -------------
+    
+    1.---  
+    The applied current (I_load) and the time (t) can be vectors (ndarrays)
+    to calculate the entire discharge curve of the battery. However, 
+    BOTH inputs must be vectors of the same length!
+    
+    2.---
+    Ensure that the inputs have the correct units! 
+    Hobby batteries are rated in mili-Amp*hr
+    This code requires you to enter the capacity in Amp*hr
+    
+    3.--- 
+    Battery terminology is imprecise. A "pack" and "module" may mean
+    different things to different people.
+
+    For example, the Thunder Power TP7700-6SR70 is a 7.7 Ah (Amp*hr) unit 
+    which consists of 6 cells in series.
+    To model this battery unit, input 7.7 for Q_Ah and set n_ser = 6
+    If you had 4 of these units wired in series, set n_ser = 4*6
+    If you had 4 of these units wired in parallel, set n_prll = 4
+    
+    4.---
+    Normally, you should not deplete a lithium-ion battery below 3.5 V
+    In extreme circumstances, you can delpete a li-ion battery down to 3.3 V 
+    *NEVER* deplete a battery below 3 V
+    """
+    
+    # convert variables to SI units
+    Q_tot = n_prll*Q_Ah #[Amp*hr], total capacity of *pack*
+    I_rated = n_prll*Q_Ah #[Amps], rated current of *pack*
+    
+    # compute Peukert effect of load (sort of safety factor)
+    pkrt_exp = np.ones(I_load.shape)
+    pkrt_exp[I_load/I_rated>1.0] = pkrt
+    I_pkrt = I_load**pkrt_exp #[A] 
+
+    # compute depth of discharge, state of charge
+    Q_out = scipy.integrate.cumtrapz(I_pkrt,t_hr,initial=0) #[A*hr]
+    Q_out[Q_out>Q_tot] = np.nan #throw out time steps where battery is overloaded
+    dod = Q_out/Q_tot #[-]
+    soc = 1-dod #[-]
+    
+    # compute SOC voltage for 1 cell, then pack
+    V_soc = (-1.031*np.exp(-35*soc) + 3.685 +
+             0.2156*soc - 0.1178*soc**2 + 0.3201*soc**3) #[V]
+    V_pack = V_soc*n_ser #[V]
+
+    # compute pack's internal resistance
+    R_eq = R_int*(n_ser/n_prll) #[Ohms], equivalent resistance of pack
+    
+    # compute terminal voltage of pack
+    V_term = V_pack - I_pkrt*R_eq #[V]
+
+    return V_term,dod,soc
+    
+def batt_size(t_hr,e,rho=2.037e3):
+    """
+    predict battery mass, size for a given duration, specific energy
+    
+    INPUTS
+    ------
+    t : float, hours
+        time duration of mission or mission phase
+    e : float, Wh/kg
+        specific energy (energy/mass) of mission or mission phase
+    rho : float, kg/m^3 (optional)
+        the mass density (mass/volume) of a lipo battery
+       
+    OUTPUTS
+    -------
+    m : float, kg
+        mass of required battery pack
+    U : float, m^3
+        volume of required battery pack
+        
+    SPECIAL NOTES
+    -------------
+    mass density (mass/volume) of typical lipo is ~2000 kg/m^3 or 2 g/cm^3
+    
+    reasonable usable energy densities:
+        200 Wh/kg for an *extremely* well-optimized low-current application
+        170 Wh/kg for a low-current (fixed-wing) application
+        140 Wh/kg for a high-current (VTOL) application
+    """
+    
+    m = t_hr/e #[kg]
+    U = m/rho #[m^3]
+
+    return m,U
--- evpy/ex_motor_esc_pred.py
+++ evpy/ex_motor_esc_pred.py
@@ -15,11 +15,13 @@ d = 50.0/100.0 #[-]
 # generate speed range based on applied voltage
 V_app = V_batt*d #[V]
 N_max = kv*V_app #[rpm]
-N_range = np.linspace(0,N_max,num=1000) #[rpm]
+N_range = np.linspace(0,N_max,num=50) #[rpm]
 w_range = N_range*(np.pi/30.0) #[rad/s]
 
 # generate and plot predictions
-T,P_out,I,P_in,n = ev.motor_pred(V_batt,d,Rm,kt,I0,w_range)
+T,P_out,I_mot,P_mot,n_mot = ev.motor_pred(w_range,V_batt,d,Rm,kt,I0)
+I_dc,P_dc,n_esc = ev.esc_pred(I_mot,P_mot,V_batt,d) 
+n_tot = n_mot*n_esc
 
 # plot results versus speed
 fig,ax = plt.subplots(1,3,sharex=True)
@@ -27,12 +29,20 @@ for axis in ax:
     axis.set_xlabel("Rotational speed [rev/min]")
     axis.grid()
 
+# motor torque-speed
 ax[0].plot(N_range,T*1e3)
 ax[0].set_ylabel("Torque [N.m]")
+
+# motor output power
 ax[1].plot(N_range,P_out,label="Output")
 ax[1].set_ylabel("Power [W]")
-ax[2].plot(N_range,n*100)
+
+# efficiencies
+ax[2].plot(N_range,n_mot*100,'--',label="Motor")
+ax[2].plot(N_range,n_esc*100,'.-',label="ESC")
+ax[2].plot(N_range,n_tot*100,label="Total")
 ax[2].set_ylabel("Efficiency [%]")
+ax[2].legend()
 
 plt.tight_layout()
 plt.show()
